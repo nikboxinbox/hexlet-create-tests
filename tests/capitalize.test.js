4f9a727537a722b08083b7c5ef19aace0b0342db
Nik Nik <nikbox88@gmail.com>
2021-12-27 13:27:46 +0300

    add assert.equal

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,17 @@
 import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'assert'
+//assert.equal(capitalize(''), '')
+
+assert.equal(capitalize('hello'), 'Hello' );
+assert.equal(capitalize(''), '');
+
  
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
   
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
   
 console.log('Все тесты пройдены!');
